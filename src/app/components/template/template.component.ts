640787a58a7d1c54f56e555d3b05735a71baa8ff
Miguel Ramos <return.infox@gmail.com>
2019-12-29 04:16:45 -0300

    formulario con select y radio, radio no dinamico

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: [`
+    .ng-invalid.ng-touched:not(form) {
+      border: 1px solid red;
+    }
+  `]
+})
+export class TemplateComponent {
+
+  usuario: Object = {
+    nombre: null,
+    apellido: null,
+    correo: null,
+    pais: "",
+    sexo: "hombre"
+  }
+
+  paises = [
+    {
+      codigo: "CL",
+      nombre: "Chile"
+    },
+    {
+      codigo: "CRI",
+      nombre: "Costa Rica"
+    },
+    {
+      codigo: "ESP",
+      nombre: "España"
+    },
+    {
+      codigo: "ARG",
+      nombre: "Argentina"
+    },
+    {
+      codigo: "CO",
+      nombre: "Colombia"
+    }
+
+  ]
+  constructor() { }
+
+  guardar(forma: NgForm) {
+
+    console.log("NgForm", forma);
+    console.log("Valor forma", forma.value)
+    console.log("Usuario", this.usuario);
+  }
+}
